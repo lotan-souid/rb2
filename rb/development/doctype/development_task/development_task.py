c3018c025c0501c4072f935d3ac7594e091d8827
lotan-souid <lotan.souid@gmail.com>
2025-08-08 14:14:46 +0000

    a coomit to make sure all is update in github

--- rb/development/doctype/development_task/development_task.py
+++ rb/development/doctype/development_task/development_task.py
@@ -2,8 +2,8 @@
 # For license information, please see license.txt
 
 # import frappe
-from frappe.utils.nestedset import NestedSet
+from frappe.model.document import Document
 
 
-class DevelopmentTask(NestedSet):
+class DevelopmentTask(Document):
 	pass
