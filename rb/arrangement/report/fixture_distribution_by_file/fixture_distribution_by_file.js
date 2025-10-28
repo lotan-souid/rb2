75ec35e51afbfc2c3b42ae94203e935616573422
lotan souid <lotan.suid@gmail.com>
2025-10-28 19:21:34 +0000

    fix: current version bugfixes (moved from wrong branch)

---
+++ rb/arrangement/report/fixture_distribution_by_file/fixture_distribution_by_file.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, lotan souid and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Fixture Distribution by File"] = {
+	"filters": [
+
+	]
+};
